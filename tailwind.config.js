b582c23cac949a1e2b77da0023c850ec42f2c3ac
Mikhail <lalkazatralen2@gmail.com>
2024-11-05 21:00:08 +0200

    navnbg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeInOut: {
+         '0%, 100%': {opacity: '0'},
+         '10%, 90%': {opacity: '1'}
+        }
+      },
+      animation: {
+        'fade-in-out': 'slide 24s infinite'
+      }
+    },
   },
   plugins: [],
 }
